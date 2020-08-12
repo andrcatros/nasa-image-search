cdb41fed2a09fae3ad84205d0a702eba7c796f12
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-08-12 11:39:35 +0100

    Initial commit, cleaned up default folders and files from CRA.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
